b544eee830f99048f938656e296a1dec4ddb0408
Kevin Kipp <kevin.kipp@gmail.com>
2016-11-14 23:48:02 -0600

    Remove console.log

--- src/app/services/cook-off.service.ts
+++ src/app/services/cook-off.service.ts
@@ -35,7 +35,6 @@ export class CookOffService {
   saveCookOff(cookOff: CookOff) {
     this._cookOff = cookOff;
     const model = JSON.parse(JSON.stringify(cookOff));
-    console.log('saving: ', model);
     this.cookOffDb.set(model);
   }
 
