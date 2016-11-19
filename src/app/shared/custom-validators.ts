ba1480d242855e5ecf7ca5e29317f264f8be607b
Kevin Kipp <kevin.kipp@gmail.com>
2016-11-19 10:00:15 -0600

    Move custom validators and add mailFormat

--- src/app/shared/custom-validators.ts
+++ src/app/shared/custom-validators.ts
@@ -1,5 +1,16 @@
 import {FormControl, ValidatorFn} from "@angular/forms";
 
+export function mailFormat(control: FormControl) {
+
+  const emailRegex = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
+
+  if (control.value != "" && (control.value.length <= 5 || !emailRegex.test(control.value))) {
+    return { "incorrectMailFormat": true };
+  }
+
+  return null;
+}
+
 export function MinNumber(num: number) : ValidatorFn {
   function MinNumber(c: FormControl) {
     if (c.value < num) {
